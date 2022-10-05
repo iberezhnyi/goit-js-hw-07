bf35b2a4a4700229fab4c3f9f8c416a9b722326b
Ihor Berezhnyi <iberezhnyi@gmail.com>
2022-10-05 11:01:22 +0300

    finished first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,18 +1,21 @@
 import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery"); // Основний дів галереї
+const galleryCardsMarkup = createGalleryCardsMarkup(galleryItems); // Змінна, в якій розмітка всієї галереї
 
-const galleryEl = document.querySelector(".gallery");
+galleryEl.innerHTML = galleryCardsMarkup; // Додаємо розмітку всієї галереї в HTML
 
-console.log(galleryEl);
+galleryEl.addEventListener("click", onPictureClick); // Реєстрація загального кліку на галерею
 
-function createGalleryCardsMarkup(galleryItemsList) {
-  return galleryItemsList
+// Повертає рядок розмітки всіх карток галереї
+function createGalleryCardsMarkup(galleryItemsArray) {
+  return galleryItemsArray
     .map(({ preview, original, description }) => {
       return `
         <div class="gallery__item">
-            <a class="gallery__link" href="large-image.jpg">
+            <a class="gallery__link" href="${original}">
                 <img
                 class="gallery__image"
                 src="${preview}"
@@ -25,4 +28,39 @@ function createGalleryCardsMarkup(galleryItemsList) {
     .join("");
 }
 
-console.log(createGalleryCardsMarkup(galleryItems));
+// **********************************************************************
+
+function onPictureClick(e) {
+  const largeImage = e.target.dataset.source;
+  const descrImage = e.target.alt;
+
+  e.preventDefault();
+
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  // **********************************************************************
+
+  const modalFullImage = basicLightbox.create(`
+      <img src="${largeImage}" alt="${descrImage}">
+  `);
+
+  modalFullImage.show();
+
+  // **********************************************************************
+
+  window.addEventListener("keydown", onEscKeyPress);
+
+  function onEscKeyPress(e) {
+    // console.log(e);
+    if (e.code === "Escape") {
+      modalFullImage.close();
+    }
+    if (!modalFullImage.visible()) {
+      window.removeEventListener("keydown", onEscKeyPress);
+    }
+  }
+
+  // **********************************************************************
+}
