303f31bdc706bc5fc5d2110d383b3a85f96b89fe
Ihor Berezhnyi <iberezhnyi@gmail.com>
2022-10-06 21:02:50 +0300

    fixed first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,19 +2,24 @@ import { galleryItems } from "./gallery-items.js";
 
 // Change code below this line
 
-const galleryEl = document.querySelector(".gallery"); // Основний дів галереї
-const galleryCardsMarkup = createGalleryCardsMarkup(galleryItems); // Змінна, в якій розмітка всієї галереї
+const galleryEl = document.querySelector(".gallery");
+const galleryCardsMarkup = createGalleryCardsMarkup(galleryItems);
 
-galleryEl.innerHTML = galleryCardsMarkup; // Додаємо розмітку всієї галереї в HTML
+const getImage = (e) => {
+  const large = e.target.dataset.source;
+  const desc = e.target.alt;
+  return { large: large, desc: desc };
+};
 
-galleryEl.addEventListener("click", onPictureClick); // Реєстрація загального кліку на галерею
+galleryEl.addEventListener("click", onPictureClick);
 
-// Повертає рядок розмітки всіх карток галереї
+galleryEl.innerHTML = galleryCardsMarkup;
+
 function createGalleryCardsMarkup(galleryItemsArray) {
   return galleryItemsArray
-    .map(({ preview, original, description }) => {
-      return `
-        <div class="gallery__item">
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
             <a class="gallery__link" href="${original}">
                 <img
                 class="gallery__image"
@@ -23,44 +28,52 @@ function createGalleryCardsMarkup(galleryItemsArray) {
                 alt="${description}"
                 />
             </a>
-        </div>`;
-    })
+        </div>`
+    )
     .join("");
 }
 
-// **********************************************************************
-
 function onPictureClick(e) {
-  const largeImage = e.target.dataset.source;
-  const descrImage = e.target.alt;
+  const image = getImage(e);
+
+  if (!e.target.classList.contains("gallery__image")) return;
 
   e.preventDefault();
 
-  if (!e.target.classList.contains("gallery__image")) {
-    return;
-  }
-
-  // **********************************************************************
-
-  const modalFullImage = basicLightbox.create(`
-      <img src="${largeImage}" alt="${descrImage}">
-  `);
-
+  const modalFullImage = basicLightbox.create(
+    `<img src="${image.large}" alt="${image.desc}">`
+  );
   modalFullImage.show();
 
-  // **********************************************************************
-
-  window.addEventListener("keydown", onEscKeyPress);
-
-  function onEscKeyPress(e) {
-    // console.log(e);
-    if (e.code === "Escape") {
-      modalFullImage.close();
-    }
-    if (!modalFullImage.visible()) {
-      window.removeEventListener("keydown", onEscKeyPress);
-    }
-  }
-
-  // **********************************************************************
+  keyboardCloseModal(modalFullImage);
 }
+
+function keyboardCloseModal(modalImg) {
+  const onEscKey = (e) => {
+    if (e.code === "Escape") {
+      modalImg.close();
+      window.removeEventListener("keydown", onEscKey);
+      // console.log(e);
+    }
+    return;
+  };
+  window.addEventListener("keydown", onEscKey);
+}
+
+// function keyboardCloseModal(modalImg) {
+//   const onEscKey = (e) => {
+//     if (e.code === "Escape") {
+//       modalImg.close();
+//       // window.removeEventListener("keydown", onEscKey);
+//       removeListenerIfNotVisible(modalImg, onEscKey);
+//       console.log(e);
+//     } else {
+//       return;
+//     }
+//   };
+//   window.addEventListener("keydown", onEscKey);
+// }
+
+// function removeListenerIfNotVisible(modalImg, onEscKey) {
+//   if (!modalImg.visible()) window.removeEventListener("keydown", onEscKey);
+// }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery"); // Основний дів галереї
+const galleryCardsMarkup = createGalleryCardsMarkup(galleryItems); // Змінна, в якій розмітка всієї галереї
+
+galleryEl.innerHTML = galleryCardsMarkup; // Додаємо розмітку всієї галереї в HTML
+
+galleryEl.addEventListener("click", onPictureClick); // Реєстрація загального кліку на галерею
+
+// Повертає рядок розмітки всіх карток галереї
+function createGalleryCardsMarkup(galleryItemsArray) {
+  return galleryItemsArray
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image"
+        src="${preview}"
+        alt="${description}" />
+    </a>`;
+    })
+    .join("");
+}
+
+function onPictureClick(e) {
+  console.log(e.target);
+  console.log(e.currentTarget);
+}
