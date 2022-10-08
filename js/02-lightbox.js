ea098ec7acf295ca19ba6d26cf902fa35b004e4f
Ihor Berezhnyi <iberezhnyi@gmail.com>
2022-10-08 14:17:57 +0300

    finish all tasks

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,30 +1,29 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const galleryCardsMarkup = createGalleryCardsMarkup(galleryItems);
+const imgEL = document.querySelector("img");
 
-const galleryEl = document.querySelector(".gallery"); // Основний дів галереї
-const galleryCardsMarkup = createGalleryCardsMarkup(galleryItems); // Змінна, в якій розмітка всієї галереї
+galleryEl.innerHTML = galleryCardsMarkup;
 
-galleryEl.innerHTML = galleryCardsMarkup; // Додаємо розмітку всієї галереї в HTML
+const fullImage = new SimpleLightbox(".gallery .gallery__item", {
+  captionPosition: "bottom",
+  captionsData: "alt",
+  captionDelay: 250,
+});
 
-galleryEl.addEventListener("click", onPictureClick); // Реєстрація загального кліку на галерею
-
-// Повертає рядок розмітки всіх карток галереї
 function createGalleryCardsMarkup(galleryItemsArray) {
   return galleryItemsArray
     .map(({ preview, original, description }) => {
       return `
-    <a class="gallery__item" href="${original}">
-        <img class="gallery__image"
-        src="${preview}"
-        alt="${description}" />
-    </a>`;
+    <li>
+      <a class="gallery__item" href="${original}">
+          <img class="gallery__image"
+          src="${preview}"
+          alt="${description}" />
+      </a>
+    </li>`;
     })
     .join("");
-}
-
-function onPictureClick(e) {
-  console.log(e.target);
-  console.log(e.currentTarget);
 }
